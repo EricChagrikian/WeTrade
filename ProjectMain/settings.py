afc49cd5bbf81aa79f228a28449dc4048cfe8023
Eric Chagrikian <echagrikian@hotmail.com>
2022-07-18 16:42:31 +0200

    ACTUAL little fix

--- ProjectMain/settings.py
+++ ProjectMain/settings.py
@@ -26,7 +26,7 @@ environ.Env.read_env()
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-kr7x!(ahh^=l))+fj@s4t*k1&^q%rdq8(h)#p36^mubo@xv2lb'
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
