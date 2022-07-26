0795ae7bceec41898373c9e2d0717c38d3a6a40e
Antoine <tanguy.hartiel.1@gmail.com>
2022-07-26 11:27:30 +0200

    uptade trading reste plus cas renvoyé les api

--- trackingAPI/models.py
+++ trackingAPI/models.py
@@ -2,11 +2,11 @@ from django.db import models
 
 # Create your models here.
 
-class cryptocurrency(models.Model):
+class Cryptocurrency(models.Model):
     cryptocurrency = models.CharField(max_length=100)
     price = models.CharField(max_length=100)
     market_cap = models.CharField(max_length=100)
-    change = models.CharField(max_length=100)
+    #change = models.CharField(max_length=100)
 
     def __str__(self):
         return self.cryptocurrency
